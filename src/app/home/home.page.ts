5a5498067598212a9537e5a887645dfb8cd2916a
FelipMoraes <50123467+FelipMoraes@users.noreply.github.com>
2020-02-17 22:21:27 -0300

    Adicionado lista dinâmica
    Removido o HTML dos Pokémons
    Criado o array com os Pokémons
    Utilizado o for para exibir os dados do array

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public listaPokemons = [
+    /* Bulbasaur */
+    {numero: '001', nome: 'Bulbasaur', tipos: ['Grass', 'Poison'], img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'},
+
+    /* Charmander */
+    {numero: '004', nome: 'Charmander', tipos: ['Fire'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'},
+
+    /* Squirtle */ 
+    {numero: '007', nome: 'Squirtle', tipos: ['Water'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'},
+
+    /* Pikachu */ 
+    {numero: '025', nome: 'Pikachu', tipos: ['Eletric'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'},
+
+    /* Zapdos */ 
+    {numero: '145', nome: 'Zapdos', tipos: ['Eletric','Flying'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/145.png'},
+
+    /* Entei */
+    {numero: '244', nome: 'Entei', tipos: ['Fire'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/244.png'},
+
+    /* Kyogre */ 
+    {numero: '382', nome: 'Kyogre', tipos: ['Water'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/382.png'},
+
+    /* Lugia */
+    {numero: '249', nome: 'Lugia', tipos: ['Psychic','Flying'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/249.png'},
+
+    /* Suicune */ 
+    {numero: '245', nome: 'Suicune', tipos: ['Water'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/245.png'},
+
+    /* Raikou */ 
+    {numero: '243', nome: 'Raikou', tipos: ['Eletric'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/243.png'}
+  ];
+  public listaFiltrada = [];
+
   constructor() {}
 
 }
